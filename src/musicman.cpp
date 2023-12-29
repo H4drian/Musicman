b10c6cffd68f86b2aacad6c390dc970c83f6a168
Icosa <117495728+H4drian@users.noreply.github.com>
2023-12-28 22:01:10 -0500

    Update musicman.cpp

--- src/musicman.cpp
+++ src/musicman.cpp
@@ -1,111 +1,89 @@
 #include "musicman.h"
 
-Buzzer::Buzzer(int p, BuzzerType bt)
-{
-    pin = p;
-    buzzerType = bt;
+Buzzer::Buzzer(int p, BuzzerType bt) {
+  pin = p;
+  buzzerType = bt;
 
-    pinMode(pin, OUTPUT);
+  pinMode(pin, OUTPUT);
 }
 
-void Buzzer::setPin(int p)
-{
-    pin = p;
+void Buzzer::setPin(int p) {
+  pin = p;
 }
 
-void Buzzer::setBuzzerType(BuzzerType bt)
-{
-    buzzerType = bt;
+void Buzzer::setBuzzerType(BuzzerType bt) {
+  buzzerType = bt;
 }
 
-void Buzzer::play(Notes note, NoteType noteType, bool isDotted)
-{
-    const long dur = getDuration(noteType, isDotted);
+void Buzzer::play(Notes note, NoteType noteType, bool isDotted) {
+  const long dur = getDuration(noteType, isDotted);
 
-    if (buzzerType == ACTIVE)
-    {
-        if (dur == -1)
-        {
-            tone(pin, note);
-        }
-        else
-        {
-            tone(pin, note, dur);
-        }
+  if (buzzerType == ACTIVE) {
+    if (dur == -1) {
+      tone(pin, note);
+    } else {
+      tone(pin, note, dur);
     }
-    else if (buzzerType == PASSIVE)
-    {
-        constexpr unsigned long halfPeriodMicros = 1000000 / (Notes::C4 *2);	// Assuming C4 as reference note
+  } else if (buzzerType == PASSIVE) {
+    constexpr unsigned long halfPeriodMicros = 1000000 / (Notes::C4 * 2); // Assuming C4 as reference note
 
-        if (dur == -1)
-        {
-            while (true)
-            {
-                digitalWrite(pin, HIGH);
-                delayMicroseconds(halfPeriodMicros);
-                digitalWrite(pin, LOW);
-                delayMicroseconds(halfPeriodMicros);
-            }
-        }
-        else
-        {
-            const unsigned long endTime = millis() + dur;
-            while (millis() < endTime)
-            {
-                digitalWrite(pin, HIGH);
-                delayMicroseconds(halfPeriodMicros);
-                digitalWrite(pin, LOW);
-                delayMicroseconds(halfPeriodMicros);
-            }
-        }
-    }
-}
-
-void Buzzer::off()
-{
-    if (buzzerType == ACTIVE)
-    {
-        noTone(pin);
-    }
-    else if (buzzerType == PASSIVE)
-    {
+    if (dur == -1) {
+      while (true) {
+        digitalWrite(pin, HIGH);
+        delayMicroseconds(halfPeriodMicros);
         digitalWrite(pin, LOW);
+        delayMicroseconds(halfPeriodMicros);
+      }
+    } else {
+      const unsigned long endTime = millis() + dur;
+      while (millis() < endTime) {
+        digitalWrite(pin, HIGH);
+        delayMicroseconds(halfPeriodMicros);
+        digitalWrite(pin, LOW);
+        delayMicroseconds(halfPeriodMicros);
+      }
     }
+  }
 }
 
-double getNoteDuration(NoteType noteType, bool isDotted = false)
-{
-    const double beatsPerMinute = static_cast<double> (tempo);
-    const double quarterNoteDuration = 60.0 / beatsPerMinute;
-    double duration = 0.0;
+void Buzzer::off() {
+  if (buzzerType == ACTIVE) {
+    noTone(pin);
+  } else if (buzzerType == PASSIVE) {
+    digitalWrite(pin, LOW);
+  }
+}
 
-    switch (noteType)
-    {
-        case WHOLE:
-            duration = quarterNoteDuration * 4;
-            break;
-        case HALF:
-            duration = quarterNoteDuration * 2;
-            break;
-        case QUARTER:
-            duration = quarterNoteDuration;
-            break;
-        case EIGHTH:
-            duration = quarterNoteDuration / 2;
-            break;
-        case SIXTEENTH:
-            duration = quarterNoteDuration / 4;
-            break;
-        default:
-            std::cerr << "Invalid note type\n";
-            break;
-    }
+double getNoteDuration(NoteType noteType, bool isDotted = false) {
+  const double beatsPerMinute = static_cast < double > (tempo);
+  const double quarterNoteDuration = 60.0 / beatsPerMinute;
+  double duration = 0.0;
 
-    if (isDotted)
-    {
-       	// Increase the duration for dotted notes
-        duration += duration / 2.0;
-    }
+  switch (noteType) {
+  case WHOLE:
+    duration = quarterNoteDuration * 4;
+    break;
+  case HALF:
+    duration = quarterNoteDuration * 2;
+    break;
+  case QUARTER:
+    duration = quarterNoteDuration;
+    break;
+  case EIGHTH:
+    duration = quarterNoteDuration / 2;
+    break;
+  case SIXTEENTH:
+    duration = quarterNoteDuration / 4;
+    break;
+  default:
+    std::cerr << "Invalid note type\n";
+    break;
+  }
 
-    return duration;
+  if (isDotted) {
+    // Increase the duration for dotted notes
+    duration += duration / 2.0;
+  }
+
+  return duration;
 }
